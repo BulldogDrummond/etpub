2658c3c86eb21c9d18eb7c93549e72e8ce30cc46
quad <quad@843fd666-81f2-4710-8602-7759cdb5ad05>
2008-09-13 01:07:43 +0000

    etpubc-20080913 version change for nightly build
    git-svn-id: http://subversion.assembla.com/svn/etpub@80 843fd666-81f2-4710-8602-7759cdb5ad05

--- trunk/src/cgame/etpubc.h
+++ trunk/src/cgame/etpubc.h
@@ -1,7 +1,7 @@
 #ifndef _ETPUBC_H
 #define _ETPUBC_H
 
-#define ETPUBC_VERSION "20080801"
+#define ETPUBC_VERSION "20080913"
 #define ETPUB_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
 
 #endif // ifndef _ETPUBC_H
