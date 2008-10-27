0ee36883fc1ceb8579ae47d56fb4ce82fd78b1bc
quad <quad@843fd666-81f2-4710-8602-7759cdb5ad05>
2008-10-27 13:38:31 +0000

    Changed etpub client version, for 20081027 nightly.
    git-svn-id: http://subversion.assembla.com/svn/etpub@88 843fd666-81f2-4710-8602-7759cdb5ad05

--- trunk/src/cgame/etpubc.h
+++ trunk/src/cgame/etpubc.h
@@ -1,7 +1,7 @@
 #ifndef _ETPUBC_H
 #define _ETPUBC_H
 
-#define ETPUBC_VERSION "20080913"
+#define ETPUBC_VERSION "20081027"
 #define ETPUB_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
 
 #endif // ifndef _ETPUBC_H
