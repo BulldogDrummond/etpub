266195fc22b6599417b2eac8beafa6893eaa65da
pheno <pheno@843fd666-81f2-4710-8602-7759cdb5ad05>
2009-02-21 00:34:11 +0000

    oops, code improvements
    git-svn-id: http://subversion.assembla.com/svn/etpub@163 843fd666-81f2-4710-8602-7759cdb5ad05

--- trunk/src/game/g_cmds.c
+++ trunk/src/game/g_cmds.c
@@ -560,7 +560,7 @@ qboolean G_SendScore_Add(gentity_t *ent, int i, char *buf, int bufsize)
 	// pheno: set misc score flags
 	miscScoreFlags = 0;
 
-	if( g_entities[level.sortedClients[i]].client->pers.ready ) {
+	if( cl->pers.ready ) {
 		miscScoreFlags |= MSF_READY;
 	}
 
